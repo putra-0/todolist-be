f860521a7ec6d7712d9113e88a368841134880aa
Putra-0 <putrasa1219@gmail.com>
2025-12-23 10:15:53 +0700

    configure global HTTP options and update model guarding settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::shouldBeStrict();
+
+        Http::globalOptions([
+            'connect_timeout' => 5,
+            'timeout' => 15,
+        ]);
     }
 }
